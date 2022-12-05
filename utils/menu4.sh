0e29dca2beb432db62539cf8fd95bf7832340bed
vaudois <info@vaudois.ch>
2022-12-06 00:19:37 +0100

    Update menu4.sh

--- utils/menu4.sh
+++ utils/menu4.sh
@@ -15,23 +15,78 @@ fi
 
 source ${absolutepath}/${installtoserver}/conf/info.sh
 
-if [[ ("${LATESTVER}" != "${VERSION}" && "${LATESTVER}" != "null") ]]; then
+if [[ ("${LATESTVER}" > "${VERSION}" && "${LATESTVER}" != "null") ]]; then
 	message_box " Updating This script to ${LATESTVER}" \
 	"You are currently using version ${VERSION}
 	\n\nAre you going to update it to the version ${LATESTVER}"
+	TAG="${LATESTVER}"
+
+	cd ~
+	clear
+
+	hide_output sudo ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+	hide_output sudo git config --global url."https://github.com/".insteadOf git@github.com:
+	hide_output sudo git config --global url."https://".insteadOf git://
+	sleep 1
+
+	REPO="vaudois/daemoncoin-addport-stratum"
+
+	temp_dir="$(mktemp -d)" && \
+		git clone -q git@github.com:${REPO%.git} "${temp_dir}" && \
+			cd "${temp_dir}/" && \
+				git -c advice.detachedHead=false checkout -q tags/${TAG}
+	sleep 1
+	test $? -eq 0 ||
+		{ 
+			echo
+			echo -e "$RED Error cloning repository. $COL_RESET";
+			echo
+			sudo rm -f $temp_dir
+			hide_output sudo git config --global url."git@github.com:".insteadOf https://github.com/
+			hide_output sudo git config --global url."git://".insteadOf https://
+			exit 1;
+		}
+	
+	hide_output sudo chown -R $USER ${temp_dir}
+	sleep 1
+	cd ${temp_dir}
+	sudo find . -type f -name "*.sh" -exec chmod -R +x {} \;
+	sleep 1
+	./install.sh "${temp_dir}"
+
+	hide_output sudo git config --global url."git@github.com:".insteadOf https://github.com/
+	hide_output sudo git config --global url."git://".insteadOf https://
+
+	sudo rm -f $temp_dir
+
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo -e "$RED    Thank you using this scrpt Updating is Finish!				 				$COL_RESET"
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo
+	cd ~
+	exit
+
 else
 	message_box " Updating This script " \
 	"Check if this scrypt needs update.
 	\n\nYou are currently using version ${VERSION}"
+
+	cd ~
+	clear
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo -e "$RED    Thank you using this scrpt!			 				$COL_RESET"
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo -e "$GREEN	Donations are welcome at wallets below:					  					$COL_RESET"
+	echo -e "$YELLOW  BTC: $COL_RESET $MAGENTA ${BTCDEP}	$COL_RESET"
+	echo -e "$YELLOW  LTC: $COL_RESET $MAGENTA ${LTCDEP}	$COL_RESET"
+	echo -e "$YELLOW  ETH: $COL_RESET $MAGENTA ${ETHDEP}	$COL_RESET"
+	echo -e "$YELLOW  BCH: $COL_RESET $MAGENTA ${BCHDEP}	$COL_RESET"
+	echo -e "$CYAN  -------------------------------------------------------------------------- 	$COL_RESET"
+	echo
+	cd ~
+	exit
+
 fi
-
-cd ~
-clear
-
-sudo curl https://raw.githubusercontent.com/vaudois/install_DmcAddpStrm/master/bootstrap.sh | bash
-
-echo -e "$CYAN --------------------------------------------------------------------------- 	$COL_RESET"
-echo -e "$RED    Thank you using this scrpt Updating is Finish!				 				$COL_RESET"
-echo -e "$CYAN --------------------------------------------------------------------------- 	$COL_RESET"
-exit
 
