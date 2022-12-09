24f8f3740369c8e4803e88aca951c46f18d1b688
vaudois <info@vaudois.ch>
2022-12-09 10:47:16 +0100

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -15,10 +15,17 @@ if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' `" == "U
   sudo chmod g-w /etc /etc/default /usr
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" == "Ubuntu 16.04 LTS" ]; then
   DISTRO=16
-elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/22\.04\.[0-9]/22.04/' `" == "Ubuntu 22.04 LTS" ]; then
+elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/20\.04\.[0-9]/20.04/' `" == "Ubuntu 20.04 LTS" ]; then
   DISTRO=22
   sudo chmod g-w /etc /etc/default /usr
+elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/22\.04\.[0-9]/22.04/' `" == "Ubuntu 22.04 LTS" ]; then
+  DISTRO=22
+    echo -e "$REDThis Script not supports on distro ${DISTRO} system stratum run on max distro Ubuntu 20.04 LTS $COL_RESET"
+    echo -e "$REDStop installation now! $COL_RESET"
+    exit
+  #sudo chmod g-w /etc /etc/default /usr
 fi
+
 
 ARCHITECTURE=$(uname -m)
 if [ "$ARCHITECTURE" != "x86_64" ]; then
