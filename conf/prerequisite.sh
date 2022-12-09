d27b5fb40c3a85e5431e1d320d920fa76388eb63
vaudois <info@vaudois.ch>
2022-12-09 10:50:35 +0100

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -5,6 +5,15 @@
 # Modified by Xavatar
 #####################################################
 
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+RED=$ESC_SEQ"31;01m"
+GREEN=$ESC_SEQ"32;01m"
+YELLOW=$ESC_SEQ"33;01m"
+BLUE=$ESC_SEQ"34;01m"
+MAGENTA=$ESC_SEQ"35;01m"
+CYAN=$ESC_SEQ"36;01m"
+
 echo
 echo
 echo -e "$CYAN => Check prerequisite : $COL_RESET"
