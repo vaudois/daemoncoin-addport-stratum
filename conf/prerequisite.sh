ed3fe5fd9b0324c4a9798dcb08f820179c7e2216
vaudois <info@vaudois.ch>
2022-12-11 01:06:50 +0100

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -21,12 +21,14 @@ if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' `" == "U
   sudo chmod g-w /etc /etc/default /usr
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" == "Ubuntu 16.04 LTS" ]; then
   DISTRO=16
+    echo -e "$REDThis Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
+    echo -e "$REDStop installation now! $COL_RESET"
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/20\.04\.[0-9]/20.04/' `" == "Ubuntu 20.04 LTS" ]; then
   DISTRO=20
   sudo chmod g-w /etc /etc/default /usr
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/22\.04\.[0-9]/22.04/' `" == "Ubuntu 22.04 LTS" ]; then
   DISTRO=22
-    echo -e "$REDThis Script not supports on distro ${DISTRO} system stratum run on max distro Ubuntu 20.04 LTS $COL_RESET"
+    echo -e "$REDThis Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
     echo -e "$REDStop installation now! $COL_RESET"
     exit
   #sudo chmod g-w /etc /etc/default /usr
