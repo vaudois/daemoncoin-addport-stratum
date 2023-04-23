840df9ae18e27820189ec0f52e2fed3c72ad62e9
vaudois <info@vaudois.ch>
2023-04-23 22:35:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@
 # web: https://coinXpool.com
 # Program:
 #   Install Daemon Coin on Ubuntu 18.04/20.04
-#   v0.7.9.9 (2023-04-17)
+#   v0.8 (2023-04-23)
 #
 ################################################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v0.7.9.9
+	TAG=v0.8
 fi
 
 clear
