774ee468fbc46e471bca2793a47a06deecc8b2b2
vaudois <info@vaudois.ch>
2023-07-20 03:35:04 +0200

    Create colors.sh

---
+++ conf/colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##################################################################
+# Current Modified by Vaudois for Daemon coin & addport & stratum
+##################################################################
+
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+RED=$ESC_SEQ"31;01m"
+GREEN=$ESC_SEQ"32;01m"
+YELLOW=$ESC_SEQ"33;01m"
+BLUE=$ESC_SEQ"34;01m"
+MAGENTA=$ESC_SEQ"35;01m"
+CYAN=$ESC_SEQ"36;01m"
