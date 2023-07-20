1ea93703925a3532b2120ba51123ffa13c343499
vaudois <info@vaudois.ch>
2023-07-20 03:34:40 +0200

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -3,15 +3,6 @@
 # Current Modified by Vaudois for Daemon coin & addport & stratum
 ##################################################################
 
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-RED=$ESC_SEQ"31;01m"
-GREEN=$ESC_SEQ"32;01m"
-YELLOW=$ESC_SEQ"33;01m"
-BLUE=$ESC_SEQ"34;01m"
-MAGENTA=$ESC_SEQ"35;01m"
-CYAN=$ESC_SEQ"36;01m"
-
 echo
 echo -e "$CYAN => Check prerequisite : $COL_RESET"
 
