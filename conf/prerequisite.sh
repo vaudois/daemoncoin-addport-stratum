49bba85e7ae6284c76e82a51f2e2f87594efddce
vaudois <info@vaudois.ch>
2023-07-20 03:33:26 +0200

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#####################################################
-# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
-# Updated by cryptopool.builders for crypto use...
-# Modified by Vaudois
-#####################################################
+##################################################################
+# Current Modified by Vaudois for Daemon coin & addport & stratum
+##################################################################
 
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
@@ -21,21 +19,24 @@ if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' `" == "U
 	DISTRO=18
  	echo -E "$YELLOW WARRING$RED php7.3 not supported on Ubuntu 18.*"
   	sleep 7
-	sudo chmod g-w /etc /etc/default /usr
+	#sudo chmod g-w /etc /etc/default /usr
+     	echo -e "$RED Stop installation now! $COL_RESET"
+	sudo rm -rf 
+ 	exit
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" == "Ubuntu 16.04 LTS" ]; then
-  DISTRO=16
-    echo -e "$RED This Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
-    echo -e "$RED Stop installation now! $COL_RESET"
+  	DISTRO=16
+    	echo -e "$RED This Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
+    	echo -e "$RED Stop installation now! $COL_RESET"
 	exit
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/20\.04\.[0-9]/20.04/' `" == "Ubuntu 20.04 LTS" ]; then
 	DISTRO=20
-	sudo chmod g-w /etc /etc/default /usr
+	#sudo chmod g-w /etc /etc/default /usr
 elif [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/22\.04\.[0-9]/22.04/' `" == "Ubuntu 22.04 LTS" ]; then
 	DISTRO=22
-    echo -e "$RED This Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
-    echo -e "$RED Stop installation now! $COL_RESET"
+    	echo -e "$RED This Script not supports on distro ${DISTRO} This run on Ubuntu 18.04 LTS and Ubuntu 20.04 LTS $COL_RESET"
+    	echo -e "$RED Stop installation now! $COL_RESET"
 	exit
-  #sudo chmod g-w /etc /etc/default /usr
+ 	#sudo chmod g-w /etc /etc/default /usr
 fi
 
 ARCHITECTURE=$(uname -m)
